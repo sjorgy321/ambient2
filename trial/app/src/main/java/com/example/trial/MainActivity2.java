bba8ad78c21ec7b01079a1d0cbf558e002541ebe
sjorgy321 <spencercjorgensen@gmail.com>
2023-10-23 23:14:56 -0600

    yuh

--- trial/app/src/main/java/com/example/trial/MainActivity2.java
+++ trial/app/src/main/java/com/example/trial/MainActivity2.java
@@ -7,25 +7,38 @@ import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothProfile;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import java.util.Random;
+import java.util.UUID;
 
-public class MainActivity2 extends AppCompatActivity {
+import kotlinx.coroutines.channels.BroadcastChannel;
+ public class MainActivity2 extends AppCompatActivity {
 
     private TextView heartRateTextView;
     private View redDotView;
     private Handler handler;
-    private Runnable heartRateUpdater;
     private BluetoothManager bluetoothManager;
     private BluetoothAdapter bluetoothAdapter;
+    private BluetoothGatt bluetoothGatt;
+    private UUID ambientHeartRateServiceUUID = UUID.fromString("0000180D-0000-1000-8000-00805F9B34FB");
+    private UUID hrRateCharacteristicUUID = UUID.fromString("00002A37-0000-1000-8000-00805F9B34FB");
+    private boolean connecting = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,56 +46,59 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         bluetoothAdapter = bluetoothManager.getAdapter();
-
         heartRateTextView = findViewById(R.id.heartRateTextView);
         redDotView = findViewById(R.id.redDot);
         handler = new Handler();
 
-        heartRateUpdater = new Runnable() {
-            // Inside the onCreate method
-            ScanCallback scanCallback = new ScanCallback() {
-                @SuppressLint("MissingPermission")
-                @Override
-                public void onScanResult(int callbackType, ScanResult result) {
-                    super.onScanResult(callbackType, result);
+        // Initialize with "trying to connect" message
+        heartRateTextView.setText("Trying to connect...");
 
-                    // Check if the scan result matches the Arduino's advertised name
-                    if (result.getDevice().getName() != null && result.getDevice().getName().equals("AmbientMonitor")) {
-                        // Stop scanning
-                        bluetoothAdapter.getBluetoothLeScanner().stopScan(this);
+        //Start scanning for BLE devices
+        startScan();
+    }
 
-                        // Connect to the Arduino
-                        connectToArduino(result.getDevice());
-                    }
-
-                }
-            };
-            
-
-// Start scanning for BLE devices
-            bluetoothAdapter.getBluetoothLeScanner().startScan(scanCallback);
-
+    private void startScan() {
+        ScanCallback scanCallback = new ScanCallback() {
             @Override
-            public void run() {
-                // Generate a random heart rate value between 100 and 120
-                int randomHeartRate = new Random().nextInt(21) + 100;
-                heartRateTextView.setText("Heart Rate: " + randomHeartRate + " BPM");
+            public void onScanResult(int callbackType, ScanResult result) {
+                super.onScanResult(callbackType, result);
 
-                // Update the Y position of the red dot based on heart rate
-                int dotPositionY = randomHeartRate * 5; // You can adjust the scaling factor
-                redDotView.setY(dotPositionY);
+                // Check if the scan result matches the Arduino's advertised name
+                if (ActivityCompat.checkSelfPermission(MainActivity2.this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                if (result.getDevice().getName() != null && result.getDevice().getName().equals("AmbientMonitor")) {
+                    // Stop scanning
+                    bluetoothAdapter.getBluetoothLeScanner().stopScan(this);
 
-                // Schedule the update every 1000ms (1 second)
-                handler.postDelayed(this, 100);
+                    // Set the connecting flag to true
+                    connecting = true;
+
+                    // Connect to the Arduino
+                    connectToArduino(result.getDevice());
+                }
             }
         };
 
-        // Start updating the heart rate and red dot position
-        handler.post(heartRateUpdater);
-
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        bluetoothAdapter.getBluetoothLeScanner().startScan(scanCallback);
     }
-
-    private BluetoothGatt bluetoothGatt;
 
     @SuppressLint("MissingPermission")
     private void connectToArduino(BluetoothDevice device) {
@@ -101,18 +117,38 @@ public class MainActivity2 extends AppCompatActivity {
             public void onServicesDiscovered(BluetoothGatt gatt, int status) {
                 super.onServicesDiscovered(gatt, status);
 
-                // Handle service and characteristic discovery here
-                // You can read or subscribe to the BLE characteristics to receive data
+                if (status == BluetoothGatt.GATT_SUCCESS) {
+                    BluetoothGattService service = gatt.getService(ambientHeartRateServiceUUID);
+                    if (service != null) {
+                        BluetoothGattCharacteristic characteristic = service.getCharacteristic(hrRateCharacteristicUUID);
+                        if (characteristic != null) {
+                            gatt.readCharacteristic(characteristic);
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+                super.onCharacteristicRead(gatt, characteristic, status);
+
+                if (characteristic.getUuid().equals(hrRateCharacteristicUUID)) {
+                    int heartRate = characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_UINT8, 0);
+                    // Update the UI with the received heart rate
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (connecting) {
+                                // If previously connecting, update text to show heart rate
+                                heartRateTextView.setText("Heart Rate: " + heartRate + " BPM");
+                                connecting = false; // Reset the connecting flag
+                            }
+                        }
+                    });
+                }
             }
         });
     }
-
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        // Stop the heart rate updating when the activity is destroyed
-        handler.removeCallbacks(heartRateUpdater);
-    }
 }
+
+
